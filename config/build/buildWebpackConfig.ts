52d64c2a2a884709173c33f792c39f1750e5c4c9
adzumano <zkaragoyshin1@gmail.com>
2023-08-28 22:07:33 +0500

    initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import {BuildOptions} from "./types/config";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildPlugins} from "./buildPlugins";
+import {Configuration} from 'webpack'
+import {buildDevServer} from "./buildDevServer";
+
+export const buildWebpackConfig = (options: BuildOptions): Configuration => {
+    const {mode, paths, isDev} = options;
+    return {
+        mode,
+        entry: paths.entry,
+        devtool: isDev ? 'inline-source-map' : undefined,
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
