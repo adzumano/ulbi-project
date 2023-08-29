7d29d2cae185a219b81c5bb42126eae8d1fdea24
adzumano <zkaragoyshin1@gmail.com>
2023-08-29 13:44:45 +0500

    added babel and added babel i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -40,9 +40,30 @@ export const buildLoaders = ({isDev}: BuildOptions): RuleSetRule[] => {
             },
         ],
     }
+
+    const babelLoader =  {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         tsLoaders,
         cssLoader
     ]
